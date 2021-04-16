3fbbfdee02cd19a838d3cb1c27702a4caed082a6
fenglolo <1287162649@qq.com>
2021-04-16 15:57:06 +0800

    1、MaterialTestActivity：Material Design UI设计:DrawerLayout；

--- app/src/main/java/com/fw/androidone/activity/material/MaterialTestActivity.java
+++ app/src/main/java/com/fw/androidone/activity/material/MaterialTestActivity.java
@@ -5,17 +5,22 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.fw.androidone.R;
 import com.fw.androidone.base.activity.BaseActivity;
 
 /**
- * description : Material Design UI设计:Toolbar
+ * description : Material Design UI设计:Toolbar、DrawerLayout
  * author : apple
  * date : 2021/4/15 15:44
  */
 public class MaterialTestActivity extends BaseActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,6 +36,8 @@ public class MaterialTestActivity extends BaseActivity {
             Toast.makeText(MaterialTestActivity.this, "点击了delete", Toast.LENGTH_SHORT).show();
         } else if (item.getItemId() == R.id.setting) {
             Toast.makeText(MaterialTestActivity.this, "点击了setting", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == android.R.id.home) {//HomeAsUp按钮的id永远都是android.R.id.home
+            drawerLayout.openDrawer(GravityCompat.START);//打开侧滑菜单
         }
         return true;
     }
@@ -48,6 +55,13 @@ public class MaterialTestActivity extends BaseActivity {
 
         Toolbar toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (null != actionBar) {
+            actionBar.setDisplayHomeAsUpEnabled(true);//设置ActionBar的HomeAsUp是否展示
+            actionBar.setHomeAsUpIndicator(R.mipmap.icon_menu);//设置HomeAsUp的图标
+        }
     }
 
     @Override
