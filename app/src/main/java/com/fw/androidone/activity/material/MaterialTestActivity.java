e4a7733112b75d73b07a451807576f101dbee69b
fenglolo <1287162649@qq.com>
2021-04-16 17:11:37 +0800

    1、MaterialTestActivity：Material Design UI设计:NavigationView；

--- app/src/main/java/com/fw/androidone/activity/material/MaterialTestActivity.java
+++ app/src/main/java/com/fw/androidone/activity/material/MaterialTestActivity.java
@@ -12,15 +12,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.fw.androidone.R;
 import com.fw.androidone.base.activity.BaseActivity;
+import com.google.android.material.navigation.NavigationView;
 
 /**
- * description : Material Design UI设计:Toolbar、DrawerLayout
+ * description : Material Design UI设计:Toolbar、DrawerLayout、NavigationView
  * author : apple
  * date : 2021/4/15 15:44
  */
 public class MaterialTestActivity extends BaseActivity {
 
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,6 +64,26 @@ public class MaterialTestActivity extends BaseActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);//设置ActionBar的HomeAsUp是否展示
             actionBar.setHomeAsUpIndicator(R.mipmap.icon_menu);//设置HomeAsUp的图标
         }
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_call);//设置navigationView默认选中call按钮
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_call) {
+                    Toast.makeText(MaterialTestActivity.this, "点击了call", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.nav_friends) {
+                    Toast.makeText(MaterialTestActivity.this, "点击了friends", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.nav_location) {
+                    Toast.makeText(MaterialTestActivity.this, "点击了location", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.nav_mail) {
+                    Toast.makeText(MaterialTestActivity.this, "点击了mail", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.nav_task) {
+                    Toast.makeText(MaterialTestActivity.this, "点击了task", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 
     @Override
