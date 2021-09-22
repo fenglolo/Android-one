6ef252a94ac67988b2cfac9b6865f5ada1c6d733
fenglolo <1287162649@qq.com>
2021-09-22 17:18:47 +0800

    单元测试

---
+++ app/src/main/java/com/fw/test/test/powermock/Fruit.java
@@ -0,0 +1,14 @@
+package com.fw.test.test.powermock;
+
+/**
+ * description :
+ * author : apple
+ * date : 2021/9/18 14:24
+ */
+abstract class Fruit {
+    private String fruit = "水果";
+
+    public String getFruit() {
+        return fruit;
+    }
+}
